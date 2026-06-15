40c86f8df063176ee4f418e48b55066ff7e72c31
FrancoZambuto <FrancoZambuto7@gmail.com>
2026-06-15 01:44:40 -0300

    Commit inicial: Creacion del proyecto

---
+++ src/main/java/com/desi/AcevedoOronoReigenbornZambutoApplication.java
@@ -0,0 +1,13 @@
+package com.desi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcevedoOronoReigenbornZambutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcevedoOronoReigenbornZambutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desi/AcevedoOronoReigenbornZambutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcevedoOronoReigenbornZambutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
