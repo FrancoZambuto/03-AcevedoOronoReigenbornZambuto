fa23506761c05431fb8118a0fd9de1c4164fad7e
Nachitoo321 <oronoignacio484@gmail.com>
2026-06-27 19:00:45 -0300

    HU 42 -Implementa modificacion de factura

--- src/main/java/com/desi/servicios/FacturaService.java
+++ src/main/java/com/desi/servicios/FacturaService.java
@@ -3,11 +3,16 @@ package com.desi.servicios;
 import java.util.List;
 
 import com.desi.entidades.Contrato;
+import com.desi.entidades.Factura;
 import com.desi.presentacion.FacturaForm;
 
 public interface FacturaService {
 
     void registrar(FacturaForm form);
+
+    void editar(Long id, FacturaForm form);
+
+    Factura obtenerPorId(Long id);
 
     List<Contrato> obtenerContratosActivos();
 }
